e46edff252a546b3d08f9ec3598eefd76d31b2db
Admin <Admin@DESKTOP-SN4C7E6>
2022-10-04 23:30:29 +0300

    Добавьте файлы проекта.

---
+++ GeographicObjectInterface/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+interface IGeoObject
+{
+    double X_coord { get; set; }
+    double Y_coord { get; set; }
+    string Name { get; set; }
+    string Description { get; set; }
+    void getInformation() { }
+}
+
+class River : IGeoObject
+{
+    double speed;
+    double length;
+    double x_coord;
+    double y_coord;
+    string name;
+    string description;
+
+    public double X_coord { get { return x_coord; } set { x_coord = value; } }
+    public double Y_coord { get { return y_coord; } set { y_coord = value; } }
+    public string Name { get { return name; } set { name = value; } }
+    public string Description { get { return description; } set { description = value; } }
+    void IGeoObject.getInformation()
+    {
+        Console.WriteLine($"The name of the river is {name}, it is located on {x_coord};{y_coord}. It's speed is {speed} and the total length is {length}");
+    }
+}
+
+class Mountain : IGeoObject
+{
+    double highestPoint;
+    double x_coord;
+    double y_coord;
+    string name;
+    string description;
+
+    public double X_coord { get { return x_coord; } set { x_coord = value; } }
+    public double Y_coord { get { return y_coord; } set { y_coord = value; } }
+    public string Name { get { return name; } set { name = value; } }
+    public string Description { get { return description; } set { description = value; } }
+    void IGeoObject.getInformation()
+    {
+        Console.WriteLine($"The name of the mountain is {name}, it is located on {x_coord};{y_coord}. It's highest point is {highestPoint}");
+    }
+}
---
+++ GeographicObjects/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+abstract class GeoObject
+{
+    protected double x_coord;
+    protected double y_coord;
+    protected string name;
+    protected string description;
+
+    protected virtual void getInformation() { }
+}
+
+class River : GeoObject
+{
+    double speed;
+    double length;
+
+    public void getInformation()
+    {
+        Console.WriteLine($"The name of the river is {name}, it is located on {x_coord};{y_coord}. It's speed is {speed} and the total length is {length}");
+    }
+}
+
+class Mountain : GeoObject
+{
+    double highestPoint;
+}
